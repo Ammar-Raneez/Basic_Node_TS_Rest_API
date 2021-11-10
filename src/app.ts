88d201d92cd0c01fc3fc8c96902bd7a6fe45ac88
ammar <ammarraneez@gmail.com>
2021-11-10 19:05:33 +0530

    feat: basic rest api w typescript

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,14 @@
-import express from 'express';
+// require() method won't work without installing the @types packages
+// the following import syntax is required to get autocompletion
+import express, { Request, Response, NextFunction } from 'express';
+import todoRoutes from './routes/todos';
 
 const app = express();
+app.use(express.json());
+app.use('/todos', todoRoutes);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  res.status(500).json({ message: err.message });
+});
 
 app.listen(3000);
---
+++ src/routes/todos.ts
@@ -0,0 +1,16 @@
+import { Router } from 'express';
+import { createTodo, deleteTodo, getTodos, updateTodo } from '../controllers/todos';
+
+const router = Router();
+
+router
+  .route('/')
+  .post(createTodo)
+  .get(getTodos);
+
+router
+  .route('/:id')
+  .patch(updateTodo)
+  .delete(deleteTodo)
+
+export default router;
